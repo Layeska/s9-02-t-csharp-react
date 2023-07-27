25c5bb12df21f129649ef38c2a03906c7c7fd535
Ana Juárez <lisa13suarez@gmail.com>
2023-07-26 23:42:34 -0600

    Agregando any - id

--- src/hooks/useCategory.tsx
+++ src/hooks/useCategory.tsx
@@ -4,7 +4,7 @@ export default function useCategory(){
     const [selectId, setSelectId] = useState(null)
     const[selectIdOffer, setSelectIdOffer] = useState(null)
 
-    const handleSelectId = (id) => {
+    const handleSelectId = (id: any) => {
         setSelectId(id)
     }
     const handleSelectOfferId = () => {
